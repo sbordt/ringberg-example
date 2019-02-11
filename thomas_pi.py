b5b2256c425d2b140ba141fdfeb73c59cc07b717
thomasrnowak <t.nowak@t-online.de>
2019-02-11 15:28:56 +0100

    Python-Script Pi Approximation

---
+++ thomas_pi.py
@@ -0,0 +1,15 @@
+import math
+
+def estimate_pi():
+    
+    k=0
+    increment=0
+    est_pi = 0
+    
+    while abs(est_pi-math.pi)>=1e-2000:
+        increment = increment + (math.factorial(4*k)*(1103+26390*k))/((math.factorial(k)**4)*396**(4*k))
+        est_pi = 1/((2*math.sqrt(2)/9801)*increment)
+        print(est_pi)
+        k=k+1
+
+estimate_pi()
