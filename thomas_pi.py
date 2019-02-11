418c43119ab19aa38992035b2acc3023d612cb00
thomasrnowak <t.nowak@t-online.de>
2019-02-11 15:55:13 +0100

    scipt andere genauigkeit

--- thomas_pi.py
+++ thomas_pi.py
@@ -8,7 +8,7 @@ def estimate_pi():
     increment=0
     est_pi = 0
     
-    while abs(est_pi-math.pi)>=1e-2000:
+    while abs(est_pi-math.pi)>=1e-1000:
         increment = increment + (math.factorial(4*k)*(1103+26390*k))/((math.factorial(k)**4)*396**(4*k))
         est_pi = 1/((2*math.sqrt(2)/9801)*increment)
         print(est_pi)
